680398dfab9691028971bac025ca07241f2bba8e
afurgeri <alejandro.furgeri@sondeos.com.ar>
2021-03-10 12:42:55 -0300

    update

--- index.js
+++ index.js
@@ -1,8 +1,44 @@
 const challengeFunction = function(cell){
-    /* YOUR CODE HERE */
-    let result
+    return countAvailableMoviments(cell);
+}
 
-    return result
+function countAvailableMoviments(cell){
+    let letters = ["A", "B", "C", "D", "E", "F", "G", "H"];
+    let count = 0
+    let position = cell.split('');
+
+    let column = letters.indexOf(position[0]);
+    let row = parseInt(position[1]);
+
+    if(column + 1 <8) {
+        if(row + 2 <=8)
+            count++
+        if(row - 2 >=1)
+            count++
+    }
+
+    if(column + 2 <8) {
+        if(row + 1 <=8)
+            count++
+        if(row - 1 >=1)
+            count++
+    }
+
+    if(column - 1 >= 0) {
+        if(row + 2 <=8)
+            count++
+        if(row - 2 >=1)
+            count++
+    }
+    
+    if(column - 2 >= 0) {
+        if(row + 1 <=8)
+            count++
+        if(row - 1 >=1)
+            count++
+    }
+
+    return count;
 }
 
 module.exports = challengeFunction
